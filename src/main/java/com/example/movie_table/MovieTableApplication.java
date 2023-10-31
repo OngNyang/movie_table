c167dd0fcabafe144de6538ac428149bd890785a
jhj951105@gmail.com <jhj951105@gmail.com>
2023-10-31 14:20:12 +0900

    initialized

---
+++ src/main/java/com/example/movie_table/MovieTableApplication.java
@@ -0,0 +1,13 @@
+package com.example.movie_table;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTableApplication.class, args);
+	}
+
+}
